21ea9cad503bc87c9037cbe58c8fbc5d4297fe24
anders ryndel <anders_ryndel@hotmail.com>
2021-05-10 16:04:36 +0200

    reverted unrelated change

--- dcs/flyingunit.py
+++ dcs/flyingunit.py
@@ -5,7 +5,6 @@ from dcs.planes import PlaneType, A_10C
 from dcs.helicopters import HelicopterType, Ka_50
 
 import json
-import copy
 from typing import Type
 
 
@@ -65,7 +64,6 @@ class FlyingUnit(Unit):
         parking_slot.unit_id = self.id
         self.parking = parking_slot.crossroad_idx
         self.parking_id = parking_slot.slot_name
-        self.position=copy.copy(parking_slot.position)
 
     def set_property(self, prop_name, value):
         if self.addpropaircraft is None:
